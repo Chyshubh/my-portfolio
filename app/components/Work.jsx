214339ef70372ac74ccf696335c4c5c119f76c21
Chyshubh <chy.shubh@gmail.com>
2025-01-08 00:12:46 +0530

    Initial commit

--- app/components/Work.jsx
+++ app/components/Work.jsx
@@ -2,7 +2,7 @@ import { assets, workData } from '@/assets/assets'
 import Image from 'next/image'
 import React from 'react'
 
-const Work = () => {
+const Work = ({isDarkMode}) => {
     return (
         <div id='work' className="w-full px-[12%] py-10 scroll-mt-20">
             <h4 className='text-center mb-2 text-lg font-Ovo'>My Portfolio</h4>
@@ -10,7 +10,7 @@ const Work = () => {
 
             <p className="text-center max-w-2xl mx-auto mt-5 mb-12 font-Ovo">I am a frontend developer from Noida, India with 1 years of experience in multiple frameworks like React Js, Next Js, and Express Js</p>
 
-            <div className="grid grid-cols-auto my-10 gap-5">
+            <div className="grid grid-cols-auto my-10 gap-5 dark:text-black">
                 {workData.map((project, index) => (
                     <div key={index}
                         style={{backgroundImage: `url(${project.bgImage})` }} className="aspect-square bg-no-repeat bg-cover bg-center rounded-lg relative cursor-pointer group">
@@ -32,8 +32,8 @@ const Work = () => {
                 ))}
             </div>
 
-            <a href="" className="w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full py-3 px-10 mx-auto my-20 hover:bg-lightHover duration-500">
-                Show more <Image src={assets.right_arrow_bold} alt='Right arrow' className='w-4'/>
+            <a href="" className="w-max flex items-center justify-center gap-2 text-gray-700 border-[0.5px] border-gray-700 rounded-full py-3 px-10 mx-auto my-20 hover:bg-lightHover duration-500 dark:text-white dark:border-white dark:hover:bg-darkHover">
+                Show more <Image src={isDarkMode ? assets.right_arrow_bold_dark : assets.right_arrow_bold} alt='Right arrow' className='w-4'/>
             </a>
         </div>
     )
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -12,7 +12,7 @@ export default {
       colors: {
         lightHover: "#fcf4ff",
         darkHover: '#2a004a',
-        darkTheme: '11001F',
+        darkTheme: '#11001F'
       },
       fontFamily:{
         Outfit: ["Outfit", "sans-sarif"],
@@ -27,5 +27,6 @@ export default {
       }
     },
   },
+  darkMode: "selector",
   plugins: [],
 };
